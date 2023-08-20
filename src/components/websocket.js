34bb59344bf74b8034a43b0aacd9c3e6ea41dd3f
mannyagre <manuel.aguirre@potenciatujuego.mx>
2023-08-20 02:52:10 -0700

    primera implementacion de websocket

---
+++ src/components/websocket.js
@@ -0,0 +1,14 @@
+export function initWebSocket() {
+    const ws = new WebSocket('ws://10.1.2.7:5000/ws');
+  
+    ws.addEventListener('open', function (event) {
+      console.log('Conectado al servidor');
+    });
+  
+    ws.addEventListener('message', function (event) {
+      const json_data = JSON.parse(event.data);
+      console.log('Datos recibidos:', json_data);
+      // Aquí puedes actualizar tu estado o hacer lo que necesites con los datos recibidos
+    });
+  }
+  
